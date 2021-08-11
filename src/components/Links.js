e4e08839a60594b495648abb26338f535ad1c820
Michael <mkenton2020@icloud.com>
2021-08-11 16:13:10 -0500

    commit tree 31eae3d7b5428eba2e1db116b4d0e00b2a47a3fb

---
+++ src/components/Links.js
@@ -0,0 +1,12 @@
+function Links(props){ 
+    console.log(props.github)
+    return (
+    <div>
+        <h3>Links</h3>
+        <a href={props.linkedin}>{props.linkedin}</a>
+        <a href={props.github}>{props.github}</a>
+    </div>
+    );
+}
+
+export default Links
